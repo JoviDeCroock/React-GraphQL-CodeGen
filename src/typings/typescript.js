3033604aa200371ccc099f8fb6785311ef2f54e3
jdecroock <decroockjovi@gmail.com>
2019-02-10 15:34:47 +0100

    convert basic object types

--- src/typings/typescript.js
+++ src/typings/typescript.js
@@ -1,14 +1,21 @@
-const { NAME, OBJECT, NON_NULLABLE } = require('../constants');
+const { NAME, OBJECT, NON_NULLABLE, LIST_TYPE, INPUT_TYPE } = require('../constants');
 const { tsTypeMapping } = require('../constants/mapping');
 
 function generateTypes(fieldDefinitions) {
   let types = ``;
-  console.dir(fieldDefinitions, { depth: 3 });
   fieldDefinitions.forEach((def) => {
     const notNullable = def.type.kind === NON_NULLABLE;
+    let listType = def.type.kind === LIST_TYPE;
     if (notNullable) {
-      types = `${types}\n  ${def.name.value}: ${tsTypeMapping[def.type.type.name.value] || def.type.type.name.value};`;
-    } else {
+      listType = def.type.type.kind === LIST_TYPE;
+      if (listType) {
+        types = `${types}\n  ${def.name.value}: Array<${tsTypeMapping[def.type.type.type.name.value] || def.type.type.type.name.value}>;`;
+      } else {
+        types = `${types}\n  ${def.name.value}: ${tsTypeMapping[def.type.type.name.value] || def.type.type.name.value};`;
+      }
+    } else if (listType) {
+      types = `${types}\n  ${def.name.value}: Array<${tsTypeMapping[def.type.type.name.value] || def.type.type.name.value}>;`;
+    }else {
       types = `${types}\n  ${def.name.value}?: ${tsTypeMapping[def.type.name.value] || def.type.name.value};`;
     }
   });
@@ -20,9 +27,11 @@ function generateTSTypes(documentDefinitions) {
   documentDefinitions.forEach((def) => {
     switch(def.kind) {
       case OBJECT: {
-        console.log(def.name.value)
         types = `${types}type ${def.name.value} {${generateTypes(def.fields)}}\n\n`;
         break;
+      }
+      case INPUT_TYPE: {
+        console.log(def);
       }
       default: console.error('unsupported type ', def.kind);
     }
